afe91f09a7a6a2440dfac72d0335261844a07807
Toni Lamont <tlam212@gmail.com>
2019-03-27 17:21:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var result = [];
+  for(let i = 0; i < musicians.length; i++){
+    result.push(musicians[i] + " plays " + instruments[i]);
+  } 
+  return result;
+}
+
+function johnLennonFacts(facts){
+  
+  while(facts){
+    console.log(facts + "!!!")
+  }
+}
